50236728bdc3b83487a557acfc8f05e72adc3ee5
Norsoft TFS SDX <jhongalvis2@gmail.com>
2021-01-06 16:08:47 -0500

    La base de microservicios, generación de cliente con Feign y balanceo de carga con Ribbon.

---
+++ springboot-servicio-productos/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO PRODUCTOS (nombre,precio,create_at) VALUES ('Panasonic',800000,NOW());
+INSERT INTO PRODUCTOS (nombre,precio,create_at) VALUES ('Sony',810000,NOW());
+INSERT INTO PRODUCTOS (nombre,precio,create_at) VALUES ('Apple',820000,NOW());
+INSERT INTO PRODUCTOS (nombre,precio,create_at) VALUES ('Adidas',830000,NOW());
+INSERT INTO PRODUCTOS (nombre,precio,create_at) VALUES ('Reebok',840000,NOW());
